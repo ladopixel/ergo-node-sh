8f9f048612bc43bd9fd242d71ac277fd93584838
ladopixel <ladopixel@protonmail.com>
2023-08-09 15:33:18 +0200

    Update start_node. If the configuration file already exists, do not create it again.

--- start_node.sh
+++ start_node.sh
@@ -83,60 +83,59 @@ if [ ! -f "$HOME/$directory/ergo.conf" ]; then
     ergo_conf="}"
     echo $ergo_conf >> "$HOME/$directory/ergo.conf"
     echo -e "${texto_verde}[+] Configuration file successfully created ${reset}"
+
+    # I run the node for the first time
+    cd "$HOME"/"$directory" || exit
+    java -jar $jar_file --mainnet -c ergo.conf &
+
+
+    # I store the pid of the node process running at background for the first time
+    node_process_pid=$!
+    echo -e "${texto_verde}[+] PID $node_process_pid ${reset}"
+
+
+    # To initialize local node
+    sleep 20
+
+
+    # known public node http://213.239.193.208:9053/
+    # local node http://localhost:9053/
+    # Create my key haciendo uso de la API en mi nodo activo
+    api_key=$(curl -X POST "http://localhost:9053/utils/hash/blake2b" -H "accept: application/json" -H "Content-Type: application/json" -d "\"$my_key\"")
+    if [ $? -eq 0 ]; then # Check if the request was successful
+    echo -e "${texto_verde}[+] API Response: Api key generated correctly ${reset}"
+    else
+    echo -e "${texto_rojo}[-] Error when querying the API. ${reset}"
+    fi
+
+
+    # Stop the node 
+    kill $node_process_pid
+    echo -e "${texto_verde}[+] Node closed to restart with api_key ${reset}"
+
+
+    # update ergo.conf with api_key
+    echo_conf="scorex {"
+    echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
+    echo_conf="restApi {"
+    echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
+    echo_conf="apiKeyHash = $api_key"
+    echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
+    echo_conf="}"
+    echo $echo_conf >> "$HOME/$directory/ergo.conf"
+    echo_conf="network {"
+    echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
+    echo_conf="nodeName = \"ergo-at-bash\""
+    echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
+    echo_conf="agentName = \"ergo-at-bash\""
+    echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
+    echo_conf="maxConnections = 12"
+    echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
+    echo_conf="}"
+    echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
+    echo_conf="}"
+    echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
 fi
-
-
-# I run the node for the first time
-cd "$HOME"/"$directory" || exit
-java -jar $jar_file --mainnet -c ergo.conf &
-
-
-# I store the pid of the node process running at background for the first time
-node_process_pid=$!
-echo -e "${texto_verde}[+] PID $node_process_pid ${reset}"
-
-
-# To initialize local node
-sleep 20
-
-
-# known public node http://213.239.193.208:9053/
-# local node http://localhost:9053/
-# Create my key haciendo uso de la API en mi nodo activo
-api_key=$(curl -X POST "http://localhost:9053/utils/hash/blake2b" -H "accept: application/json" -H "Content-Type: application/json" -d "\"$my_key\"")
-if [ $? -eq 0 ]; then # Check if the request was successful
-  echo -e "${texto_verde}[+] API Response: Api key generated correctly ${reset}"
-else
-  echo -e "${texto_rojo}[-] Error when querying the API. ${reset}"
-fi
-
-
-# Stop the node 
-kill $node_process_pid
-echo -e "${texto_verde}[+] Node closed to restart with api_key ${reset}"
-
-
-# update ergo.conf with api_key
-echo_conf="scorex {"
-echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
-echo_conf="restApi {"
-echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
-echo_conf="apiKeyHash = $api_key"
-echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
-echo_conf="}"
-echo $echo_conf >> "$HOME/$directory/ergo.conf"
-echo_conf="network {"
-echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
-echo_conf="nodeName = \"ergo-at-bash\""
-echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
-echo_conf="agentName = \"ergo-at-bash\""
-echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
-echo_conf="maxConnections = 12"
-echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
-echo_conf="}"
-echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
-echo_conf="}"
-echo "$echo_conf" >> "$HOME/$directory/ergo.conf"
 
 
 # I start the node with the correctly configured configuration file.
---
+++ start_node_usb.ino/start_node_usb.ino.ino
@@ -0,0 +1,30 @@
+// ┌───────────────────────────┐
+// │ Developed by ladopixel    │
+// │ Execute Ergo node v5.0.13 │
+// └───────────────────────────┘
+
+
+// I am currently working on this file...
+
+
+#include <Keyboard.h>
+
+String ERGO;
+String url_star_node_sh;
+
+void setup() {
+  Keyboard.begin();
+}
+
+void loop() {
+
+  delay(2000);
+  
+  // Keyboard.print(comando_uno);
+  // Keyboard.press(KEY_RETURN);
+  // delay(1000);
+  // Keyboard.releaseAll();
+  
+  delay(15000);
+  
+}
